c5bda6d5aed8a6454f48ff85181f35e5bb11ea42
Anderson <andersonsalles@outlook.com>
2021-03-30 15:28:47 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace TesteSQS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SQS demo!");
+
+            var messages = CreateMessages();
+
+            Console.WriteLine("Creating queue!");
+
+            AmazonSQSClient sqsClient = new AmazonSQSClient(Amazon.RegionEndpoint.APSoutheast2);
+            string queueUrl = CreateQueue(sqsClient, "ReportsQueue", "10");
+        }
+
+        private static string CreateQueue(AmazonSQSClient sqsClient, string reportsqueue, string s)
+        {
+            return "";
+        }
+
+        private static IEnumerable<ReportFilters> CreateMessages()
+        {
+            var listToReturn = new List<ReportFilters>();
+            for (var i = 0; i < 100; i++)
+            {
+                listToReturn.Add(new ReportFilters
+                {
+                    DataIni = DateTime.Now.AddDays(i),
+                    DataEnd = DateTime.Now.AddDays(i+1)
+                });
+            }
+
+            return listToReturn;
+        }
+    }
+}
---
+++ ReportFilters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace TesteSQS
+{
+    public class ReportFilters
+    {
+        public DateTime DataIni { get; set; }
+        public DateTime DataEnd { get; set; }
+
+    }
+}
